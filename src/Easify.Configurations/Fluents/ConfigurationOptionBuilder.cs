37654d9aaf94461d6b9a8db6198e940f350f6902
Mohammad Moattar <moattarwork@msn.com>
2021-03-05 12:03:02 +0000

    Remove extra using

--- src/Easify.Configurations/Fluents/ConfigurationOptionBuilder.cs
+++ src/Easify.Configurations/Fluents/ConfigurationOptionBuilder.cs
@@ -15,7 +15,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
